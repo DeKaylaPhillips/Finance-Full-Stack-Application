08a069fbd6d19ca31af7e15cbf68fb2db46bd417
DeKayla Phillips <dekaylanicole@gmail.com>
2024-01-09 15:13:06 -0500

    Refactor Navbar component
    Replaced api call with useAuth custom hook and refactored component to use hook. Renamed Dashboard tab to "News".

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,11 @@
-import axios from "axios";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
-export default function NavBar({ firstName, lastName }) {
-  async function signOut() {
-    const response = await axios.post("/api/signOut/")
-    console.log(response)
-  }
+export default function NavBar() {
+  const { user, logout } = useAuth()
+  const { first_name: firstName, last_name: lastName } = user
+
   return (
     <div>
       <Navbar bg="light" variant="light" fixed="top">
@@ -14,7 +13,7 @@ export default function NavBar({ firstName, lastName }) {
           <Navbar.Brand href="/dashboard">Capital Kay Finance</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/articles">
-              Dashboard
+              News
             </Nav.Link>
             <Nav.Link as={Link} to="/budgetSheet">
               Budget Sheet
@@ -30,7 +29,7 @@ export default function NavBar({ firstName, lastName }) {
                 {" "}
                 {firstName} {lastName}
               </a>{" "}
-              <a href="/" onClick={() => (signOut)}>Logout</a>
+              <a href="/" onClick={logout}>Logout</a>
             </Navbar.Text>
           </Nav>
         </Container>
